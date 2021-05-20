bcd73eed65f5d5abfbbcf78fcdc09f703de4ae30
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-05-20 09:28:56 +0800

    [modified] logger.warning -> logger.debug

--- tests/test_embeddings/test_embeddings.py
+++ tests/test_embeddings/test_embeddings.py
@@ -25,17 +25,17 @@ def test_word_embeddings_size(testcase_all, extractor):
 
 def test_word_embeddings_quality(testcase2, extractor):
     tokenized_text = testcase2["tokenized_text"]
-    logger.warning(f"token 19: {tokenized_text[19]}")  ##漫畫
-    logger.warning(f"token 22: {tokenized_text[22]}")  ##漫畫
+    logger.debug(f"token 19: {tokenized_text[19]}")  ##漫畫
+    logger.debug(f"token 22: {tokenized_text[22]}")  ##漫畫
     word_embeddings1 = extractor.word_embeddings_from_text(tokenized_text, 19)
     word_embeddings2 = extractor.word_embeddings_from_text(tokenized_text, 22)
     word_embeddings3 = extractor.word_embeddings("漫畫")
     cs_score12 = ev.cosineSimilarity(word_embeddings1, word_embeddings2)
     cs_score13 = ev.cosineSimilarity(word_embeddings1, word_embeddings3)
     cs_score23 = ev.cosineSimilarity(word_embeddings2, word_embeddings3)
-    logger.warning(f"cs_score12: {cs_score12}")
-    logger.warning(f"cs_score13: {cs_score13}")
-    logger.warning(f"cs_score23: {cs_score23}")
+    logger.debug(f"cs_score12: {cs_score12}")
+    logger.debug(f"cs_score13: {cs_score13}")
+    logger.debug(f"cs_score23: {cs_score23}")
     assert cs_score12 >= 0.80
     assert cs_score13 >= 0.80
     assert cs_score23 >= 0.80
